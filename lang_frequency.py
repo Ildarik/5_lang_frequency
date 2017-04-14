9b60ad0e180e70fedc8c4fcec20efb5d3d149d8a
Nogmanov Ildar <nogmanov@gmail.com>
2017-04-14 08:19:05 +0300

    Fix issues

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,27 +2,24 @@ from collections import Counter
 from sys import argv
 import string 
 
-filepath = argv[1]
+filepath = argv[1] 
 
 def load_data(filepath):
     with open(filepath, "r") as textfile:
         text = textfile.read()
     return text
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, count):
 	# remove punctuation marks
-    clean = text.translate(str.maketrans("", "", string.punctuation))
-    words = clean.split()
-    #create collection
-    category = Counter(words)
-
-    # return most_freq_words
-    print("10 самых популярных слов в файле:\n{}".format(filepath))
-    for value, count in category.most_common(10):
-        print(count, value)
+    clean_text = text.translate(str.maketrans("", "", string.punctuation))
+    words = clean_text.split()
+    collection = Counter(words)
+    return collection.most_common(count)
 
 
 if __name__ == '__main__':
-    text = load_data(filepath)
-    #print
-    print(get_most_frequent_words(text))
+    text = load_data(filepath)    
+    top_number = 10
+    print("{} самых популярных слов в файле:\n{}".format(top_number, filepath))
+    for value, count in get_most_frequent_words(text, top_number):
+        print(count, value)
